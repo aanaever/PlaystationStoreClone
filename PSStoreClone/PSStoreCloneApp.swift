fbf169e038f125b3d3d27a78551db352a86aaaf7
aanaever <nasty.ever18@gmail.com>
2021-06-15 00:08:05 +0300

    test

---
+++ PSStoreClone/PSStoreCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  PSStoreCloneApp.swift
+//  PSStoreClone
+//
+//  Created by Анастасия Эверстова on 15.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct PSStoreCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
